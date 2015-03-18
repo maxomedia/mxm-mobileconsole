2cb1adab5b4f9194338af200f5911959bca208df
tuelsch <tuelsch@gmail.com>
2015-03-18 15:16:41 +0100

    Externalize CSS

--- devtool-mobile-console.js
+++ devtool-mobile-console.js
@@ -81,93 +81,21 @@
 		if(!$console) {
 			var consoleContainer 	= document.createElement('div');
 			var consoleInner 		= document.createElement('div');
-			var consoleStyle 		= document.createElement('style');
-
-			var css 				= '';
-
-			css += '#console {';
-			css += '	position: fixed;';
-			css += '	bottom: 0;';
-			css += '	left: 0;';
-			css += '	padding: 0em 1em;';
-			css += '	width: 100%;';
-			css += '	background-color: rgba(255,255,255,0.9);';
-			css += '	font-family: "courier new", courier, monospace;';
-			css += '	font-size: 11px;';
-			css += '	color: #000;';
-			css += '	line-height: 1em;';
-			css += '	z-index: 100000;';
-			css += '	-webkit-transition: all 400ms ease-out; -moz-transition: all 400ms ease-out; -ms-transition: all 400ms ease-out; -o-transition: all 400ms ease-out; transition: all 400ms ease-out;';
-			css += '}';
-
-			css += '#console.top {';
-			css += '	bottom: auto;';
-			css += '	top: 0;';
-			css += '}';
-
-			css += '#console:before {';
-			css += '	display: block;';
-			css += '	content: "mobile Console";';
-			css += '	position: absolute;';
-			css += '	bottom: 100%;';
-			css += '	left: 25%;';
-			css += '	right: 25%;';
-			css += '	background-color: #fff;';
-			css += '	border: solid #999;';
-			css += '	border-width: 1px 0 0;';
-			css += '	font-size: 9px;';
-			css += '	text-align: center;';
-			css += '	-webkit-border-radius: 50% 50% 0 0; -moz-border-radius: 50% 50% 0 0; -ms-border-radius: 50% 50% 0 0; -o-border-radius: 50% 50% 0 0; border-radius: 50% 50% 0 0;';
-			css += '	-webkit-transition: all 400ms ease-out; -moz-transition: all 400ms ease-out; -ms-transition: all 400ms ease-out; -o-transition: all 400ms ease-out; transition: all 400ms ease-out;';
-			css += '}';
-
-			css += '#console.top:before {';
-			css += '	bottom: auto;';
-			css += '	top: 100%;';
-			css += '	border-width: 0 0 1px;';
-			css += '	-webkit-border-radius: 0 0 50% 50%; -moz-border-radius: 0 0 50% 50%; -ms-border-radius: 0 0 50% 50%; -o-border-radius: 0 0 50% 50%; border-radius: 0 0 50% 50%;';
-			css += '}';
-
-			css += '#console > div {';
-			css += '	overflow: hidden;';
-			css += '	overflow-y: scroll;';
-			css += '	-webkit-overflow-scrolling: touch;';
-			css += '	max-height: 0em;';
-			css += '	-webkit-transition: all 400ms ease-out; -moz-transition: all 400ms ease-out; -ms-transition: all 400ms ease-out; -o-transition: all 400ms ease-out; transition: all 400ms ease-out;';
-			css += '}';
-
-			css += '#console > div > div {';
-			css += '	padding: 0.25em;';
-			css += '	border-bottom: 1px solid #ccc;';
-			css += '}';
-
-			css += '#console.visible {';
-			css += '	padding-top: 1em;';
-			css += '	padding-bottom: 1em;';
-			//css += '	-webkit-box-shadow: 0 0 20px 0 rgba(0,0,0,0.25); -moz-box-shadow: 0 0 20px 0 rgba(0,0,0,0.25); -ms-box-shadow: 0 0 20px 0 rgba(0,0,0,0.25); -o-box-shadow: 0 0 20px 0 rgba(0,0,0,0.25); box-shadow: 0 0 20px 0 rgba(0,0,0,0.25);';
-			css += '}';
-
-			css += '#console.visible:before {';
-			css += '	left: 0%;';
-			css += '	right: 0%;';
-			css += '	-webkit-box-shadow: 0 0 10px 0 rgba(0,0,0,0.25); -moz-box-shadow: 0 0 10px 0 rgba(0,0,0,0.25); -ms-box-shadow: 0 0 10px 0 rgba(0,0,0,0.25); -o-box-shadow: 0 0 10px 0 rgba(0,0,0,0.25); box-shadow: 0 0 10px 0 rgba(0,0,0,0.25);';
-			css += '}';
-
-			css += '#console.visible > div {';
-			css += '	max-height: 8em;';
-			css += '}';
+			var consoleStyle 		= document.createElement('link');
 			
 			consoleContainer.id = 'console';
 			consoleStyle.type 	= 'text/css';
+			consoleStyle.rel = 'stylesheet';
+			consoleStyle.href = 'https://raw.githubusercontent.com/maxomedia/mxm-mobileconsole/externalize/styles.less';
 			
 			if(top)
 				consoleContainer.className = 'top';
 			
-			if(consoleStyle.styleSheet) {
+			/*if(consoleStyle.styleSheet) {
 				consoleStyle.styleSheet.cssText = css;
 			} else {
 				consoleStyle.appendChild(document.createTextNode(css));
-			}
+			}*/
 
 			consoleContainer.appendChild(consoleInner);
 			consoleContainer.appendChild(consoleStyle);
